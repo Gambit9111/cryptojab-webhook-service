6f11111041366cf2ba45c5788b8fbedc8fcb627c
Gambit9111 <bitgam911@gmail.com>
2023-11-25 08:13:00 -0500

    disabled crypto webhook

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 DB_URL = os.getenv("DB_URL")
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET")
-COINBASE_ENDPOINT_SECRET = os.getenv("COINBASE_ENDPOINT_SECRET")
+# COINBASE_ENDPOINT_SECRET = os.getenv("COINBASE_ENDPOINT_SECRET")
 
 
 if DB_URL is None:
@@ -14,7 +14,7 @@ elif STRIPE_API_KEY is None:
     raise Exception("STRIPE_API_KEY not set in .env file")
 elif STRIPE_ENDPOINT_SECRET is None:
     raise Exception("STRIPE_ENDPOINT_SECRET not set in .env file")
-elif COINBASE_ENDPOINT_SECRET is None:
-    raise Exception("COINBASE_ENDPOINT_SECRET not set in .env file")
+# elif COINBASE_ENDPOINT_SECRET is None:
+#     raise Exception("COINBASE_ENDPOINT_SECRET not set in .env file")
 else :
     print("All environment variables set")
